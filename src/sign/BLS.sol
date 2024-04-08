f9d9b9950f666cca4c726142a225c38bcda1d886
Federico Gimenez <federico.gimenez@gmail.com>
2024-04-08 19:20:52 +0200

    add NatSpec comments

--- src/sign/BLS.sol
+++ src/sign/BLS.sol
@@ -1,129 +1,139 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-// Wrapper functions to abstract low level details of calls to BLS precompiles
-// defined in EIP-2537, see <https://eips.ethereum.org/EIPS/eip-2537>.
+// @title BLS
+// @notice Wrapper functions to abstract low level details of calls to BLS precompiles
+//         defined in EIP-2537, see <https://eips.ethereum.org/EIPS/eip-2537>.
+// @dev Precompile addresses come from the BLS addresses submodule in AlphaNet, see
+//      <https://github.com/paradigmxyz/alphanet/blob/main/crates/precompile/src/addresses.rs>
+//      Being addresses we can't use them as constants in inline assembly
 library BLS {
-    // Precompile addresses come from the BLS addresses submodule in AlphaNet, see:
-    // <https://github.com/paradigmxyz/alphanet/blob/main/crates/precompile/src/addresses.rs>
-
-    // Output sizes come from the ABI definition for each of the BLS operations
-    // in EIP-2537, see: <https://eips.ethereum.org/EIPS/eip-2537#abi-for-operations>
-    // G1ADD output size allocation
+    // @notice Output sizes come from the ABI definition for each of the BLS operations
+    //         in EIP-2537, see: <https://eips.ethereum.org/EIPS/eip-2537#abi-for-operations>
+    // @notice G1ADD output size allocation
     uint256 constant G1ADD_OUTPUT_SIZE = 128;
-    // G1MUL output size allocation
+    // @notice G1MUL output size allocation
     uint256 constant G1MUL_OUTPUT_SIZE = 128;
-    // G1MULTIEXP output size allocation
+    // @notice G1MULTIEXP output size allocation
     uint256 constant G1MULTIEXP_OUTPUT_SIZE = 128;
-    // G2ADD output size allocation
+    // @notice G2ADD output size allocation
     uint256 constant G2ADD_OUTPUT_SIZE = 256;
-    // G2MUL output size allocation
+    // @notice G2MUL output size allocation
     uint256 constant G2MUL_OUTPUT_SIZE = 256;
-    // G2MULTIEXP output size allocation
+    // @notice G2MULTIEXP output size allocation
     uint256 constant G2MULTIEXP_OUTPUT_SIZE = 256;
-    // PAIRING output size allocation
+    // @notice PAIRING output size allocation
     uint256 constant PAIRING_OUTPUT_SIZE = 32;
-    // MAP_FP_TO_G1 output size allocation
+    // @notice MAP_FP_TO_G1 output size allocation
     uint256 constant MAP_FP_TO_G1_OUTPUT_SIZE = 128;
-    // MAP_FP2_TO_G2 output size allocation
+    // @notice MAP_FP2_TO_G2 output size allocation
     uint256 constant MAP_FP2_TO_G2_OUTPUT_SIZE = 256;
 
-    // Function to perform G1ADD operation
-    function G1Add(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(G1ADD_OUTPUT_SIZE);
-        bool success;
+    // @notice G1ADD operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function G1Add(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(G1ADD_OUTPUT_SIZE);
         assembly {
             // G1ADD address is 0x0b
             success := staticcall(gas(), 0x0b, add(input, 0x20), mload(input), output, G1ADD_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform G1MUL operation
-    function G1Mul(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(G1MUL_OUTPUT_SIZE);
-        bool success;
+    // @notice G1MUL operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function G1Mul(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(G1MUL_OUTPUT_SIZE);
         assembly {
             // G1MUL address is 0x0c
             success := staticcall(gas(), 0x0c, add(input, 0x20), mload(input), output, G1MUL_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform G1MULTIEXP operation
-    function G1MultiExp(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(G1MULTIEXP_OUTPUT_SIZE);
-        bool success;
+    // @notice G1MULTIEXP operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function G1MultiExp(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(G1MULTIEXP_OUTPUT_SIZE);
         assembly {
             // G1MULTIEXP address is 0x0d
             success := staticcall(gas(), 0x0d, add(input, 0x20), mload(input), output, G1MULTIEXP_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform G2ADD operation
-    function G2Add(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(G2ADD_OUTPUT_SIZE);
-        bool success;
+    // @notice G2ADD operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function G2Add(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(G2ADD_OUTPUT_SIZE);
         assembly {
             // G2ADD address is 0x0e
             success := staticcall(gas(), 0x0e, add(input, 0x20), mload(input), output, G2ADD_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform G2MUL operation
-    function G2Mul(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(G2MUL_OUTPUT_SIZE);
-        bool success;
+    // @notice G2MUL operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function G2Mul(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(G2MUL_OUTPUT_SIZE);
         assembly {
             // G2MUL address is 0x0f
             success := staticcall(gas(), 0x0f, add(input, 0x20), mload(input), output, G2MUL_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform G2MULTIEXP operation
-    function G2MultiExp(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(G2MULTIEXP_OUTPUT_SIZE);
-        bool success;
+    // @notice G2MULTIEXP operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function G2MultiExp(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(G2MULTIEXP_OUTPUT_SIZE);
         assembly {
             // G2MULTIEXP address is 0x10
             success := staticcall(gas(), 0x10, add(input, 0x20), mload(input), output, G2MULTIEXP_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform PAIRING operation
-    function Pairing(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(PAIRING_OUTPUT_SIZE);
-        bool success;
+    // @notice PAIRING operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function Pairing(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(PAIRING_OUTPUT_SIZE);
         assembly {
             // PAIRING address is 0x11
             success := staticcall(gas(), 0x11, add(input, 0x20), mload(input), output, PAIRING_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform MAP_FP_TO_G1 operation
-    function MapFpToG1(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(MAP_FP_TO_G1_OUTPUT_SIZE);
-        bool success;
+    // @notice MAP_FP_TO_G1 operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function MapFpToG1(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(MAP_FP_TO_G1_OUTPUT_SIZE);
         assembly {
             // MAP_FP_TO_G1 address is 0x12
             success := staticcall(gas(), 0x12, add(input, 0x20), mload(input), output, MAP_FP_TO_G1_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 
-    // Function to perform MAP_FP2_TO_G2 operation
-    function MapFp2ToG2(bytes memory input) internal view returns (bool, bytes memory) {
-        bytes memory output = new bytes(MAP_FP2_TO_G2_OUTPUT_SIZE);
-        bool success;
+    // @notice MAP_FP2_TO_G2 operation
+    // @param input Slice of bytes representing the input for the precompile operation
+    // @return success Represents if the operation was successful
+    // @return output Result bytes of the operation
+    function MapFp2ToG2(bytes memory input) internal view returns (bool success, bytes memory output) {
+        output = new bytes(MAP_FP2_TO_G2_OUTPUT_SIZE);
         assembly {
             // MAP_FP2_TO_G2 address is 0x13
             success := staticcall(gas(), 0x13, add(input, 0x20), mload(input), output, MAP_FP2_TO_G2_OUTPUT_SIZE)
         }
-        return (success, output);
     }
 }
