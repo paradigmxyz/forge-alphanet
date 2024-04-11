60dc7921478457819a9e2e7f49e727d3b64786c8
Federico Gimenez <federico.gimenez@gmail.com>
2024-04-11 13:06:15 +0200

    move MockContract to GasSponsorInvoker.t.sol

--- test/eip3074/GasSponsorInvoker.t.sol
+++ test/eip3074/GasSponsorInvoker.t.sol
@@ -2,40 +2,46 @@
 
 pragma solidity ^0.8.23;
 
-import {Test, stdStorage, StdStorage} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import { VmSafe } from "forge-std/Vm.sol";
 import {GasSponsorInvoker} from "../../src/eip3074/GasSponsorInvoker.sol";
-import "./MockContract.sol";
+
+contract MockContract {
+    event Message(address sender, string message);
+
+    function sendMessage(string calldata message) external {
+        emit Message(msg.sender, message);
+    }
+}
 
 contract GasSponsorInvokerTest is Test {
     GasSponsorInvoker public invoker;
     MockContract public mockContract;
 
-    address private authorizer;
-    uint256 private authorizerPrivateKey = 0xabc123;
+    VmSafe.Wallet public authority;
 
     function setUp() public {
-        authorizer = vm.addr(authorizerPrivateKey);
+        authority = vm.createWallet("authority");
 
         invoker = new GasSponsorInvoker();
         mockContract = new MockContract();
-        vm.deal(authorizer, 1 ether);
     }
 
     event Message(address sender, string message);
 
     function testSponsorCall() public {
-        bytes32 commit = keccak256(abi.encodePacked("Some unique commit data"));
+        bytes32 commit = keccak256("Some unique commit data");
         string memory message = "Hello, World!";
         bytes memory data = abi.encodeWithSelector(MockContract.sendMessage.selector, message);
 
         bytes32 digest = invoker.getDigest(commit);
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(authorizerPrivateKey, digest);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(authority.privateKey, digest);
 
         vm.expectEmit(true, true, false, true);
         emit Message(address(mockContract), message);
 
-        bool success = invoker.sponsorCall(authorizer, commit, v, r, s, address(mockContract), data, 0, gasleft());
+        bool success = invoker.sponsorCall(authority.addr, commit, v, r, s, address(mockContract), data, 0, gasleft());
         assertTrue(success, "Call should be successful");
     }
 }
