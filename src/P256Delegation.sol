10514b7aa981b809ae44b03a1f81186e084bce3f
Arsenii Kulikov <klkvrr@gmail.com>
2024-08-16 03:29:03 +0800

    wip

---
+++ src/P256Delegation.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Secp256r1} from "./sign/Secp256r1.sol";
+
+contract P256Delegation {
+    uint256 authorizedPublicKeyX;
+    uint256 authorizedPublicKeyY;
+
+    uint256 nonce;
+
+    function authorize(uint256 publicKeyX, uint256 publicKeyY) public {
+        require(msg.sender == address(this));
+
+        authorizedPublicKeyX = publicKeyX;
+        authorizedPublicKeyY = publicKeyY;
+    }
+
+    function transact(address to, bytes memory data, bytes32 r, bytes32 s) public {
+        bytes32 digest = keccak256(abi.encode(nonce++, to, data));
+        require(Secp256r1.verify(digest, r, s, authorizedPublicKeyX, authorizedPublicKeyY), "Invalid signature");
+
+        (bool success,) = to.call(data);
+        require(success);
+    }
+}
